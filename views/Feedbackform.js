de4deee322166b07a7bdf541d6a17f06437e7811
ATOMworkplace <charchitg9457@gmail.com>
2023-10-04 00:46:34 +0530

    updates

--- views/Feedbackform.js
+++ views/Feedbackform.js
@@ -12,3 +12,22 @@ document.addEventListener("DOMContentLoaded", function() {
         thankYouMessage.style.display = "block";
     });
 });
+<footer class="footer">
+    <h4 class="footer-heading">FinanceCraft</h4>
+    <hr class="footer-hr">
+    <p class="footer-subheading">LEARN . ANALYSE . PREDICT</p>
+    <p class="footer-txt">Stay ahead of the market trends, make informed decisions, and maximize your returns. Join us in shaping your financial success today.</p>
+    <p class="footer-txt">. . .</p>
+    <div class="footer-icon">
+        <a target="_blank" href="https://accounts.google.com/InteractiveLogin/signinchooser?hl=en-gb&ifkv=AYZoVhcMBGRUCAY2xZqgLeMwwLpWNDmGlM9WCjVqPJcz_v1Gpks9sY8Ci299N40D5mQp41EJBHvKBA&theme=glif&flowName=GlifWebSignIn&flowEntry=ServiceLogin">
+        <img src="icons8-gmail-50.png">
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/home"></a>
+        <img src="icons8-linkedin-50.png">
+        </a>
+        <a href="#">
+        <img src="/images/i.png">
+        </a>
+    </div>
+
+</footer>
